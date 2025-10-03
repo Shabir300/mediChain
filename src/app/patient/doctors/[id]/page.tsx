d1879b83fd78f72b9c45433d346f6584dfd5ff4c
I.m Nomi <gw5531505@gmail.com>
2025-10-03 06:00:26 +0000

    Try fixing this error: `Console Error: Error: Failed to construct 'URL':

--- src/app/patient/doctors/[id]/page.tsx
+++ src/app/patient/doctors/[id]/page.tsx
@@ -98,19 +98,15 @@ export default function DoctorProfilePage() {
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                 <div className='lg:col-span-1'>
                     <Card className="overflow-hidden">
-                        
-                            <Image
-                                src={doctor.avatar}
-                                alt={`Portrait of ${doctor.name}`}
-                                width={400}
-                                height={400}
-                                className="w-full h-80 object-cover"
-                            />
-                        
+                        <Image
+                            src={doctor.avatar}
+                            alt={`Portrait of ${doctor.name}`}
+                            width={400}
+                            height={400}
+                            className="w-full h-80 object-cover"
+                        />
                         <CardHeader>
-                            <div className='flex justify-between items-start'>
-                                <CardTitle className="text-2xl font-headline">{doctor.name}</CardTitle>
-                            </div>
+                            <CardTitle className="text-2xl font-headline">{doctor.name}</CardTitle>
                             <CardDescription className='flex items-center gap-2'><Stethoscope className='h-4 w-4'/>{doctor.specialty}</CardDescription>
                         </CardHeader>
                         <CardFooter>
