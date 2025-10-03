b953045549204809f3230a3304a9b4e96d45d060
I.m Nomi <gw5531505@gmail.com>
2025-10-03 09:40:47 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,10 @@
 // This file is intentionally left blank.
 // The Firebase configuration will be populated by the backend service.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-4532126244-b9797",
+  "appId": "1:922566767257:web:ecc3ab1a8dbe5c5244fe08",
+  "apiKey": "AIzaSyA4HMqYWUE_8FaIj8SBiz4q8JrHLYurZ4A",
+  "authDomain": "studio-4532126244-b9797.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "922566767257"
+};
